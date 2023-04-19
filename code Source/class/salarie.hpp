5f4dbb32b9d6ccaeb4d3478a48e6ad07805177d5
NabilBoussalham <127595965+NabilBoussalham@users.noreply.github.com>
2023-04-20 00:39:11 +0200

    update
    add function modifierInformationSalarie()

--- code Source/class/salarie.hpp
+++ code Source/class/salarie.hpp
@@ -12,37 +12,42 @@ class salarie
 	char nomDeSalarie[20];
 	char prenomDeSalarie[20];
 	double valSalarie;
-public:	
-	int getId(){
-		 return id;
-	 };
-	 void addUnSalarie() {
+public:
+	int getId() {
+		return id;
+	};
+	void addUnSalarie() {
 		cout << "id : ";
-		cin >> this->id ;
+		cin >> this->id;
 		cout << "nom : ";
 		cin >> this->nomDeSalarie;
 		cout << "pre : ";
 		cin >> this->prenomDeSalarie;
 		cout << "sal : ";
-		cin >> this->valSalarie ;
-	} 
-// afichage
-void afficheInformationSalarie() {
-		cout << "id : " << id << endl;	
-		cout << "nom : "<< nomDeSalarie << endl;	
-		cout << "pre : "<< prenomDeSalarie <<endl;
-		cout << "sal : "<< valSalarie <<endl<<endl<<endl;
+		cin >> this->valSalarie;
 	}
-void modifierInformationSalarie() {
-
+	// afichage
+	void afficheInformationSalarie() {
+		cout << "id : " << id << endl;
+		cout << "nom : " << nomDeSalarie << endl;
+		cout << "pre : " << prenomDeSalarie << endl;
+		cout << "sal : " << valSalarie << endl << endl << endl;
+	}
+	void modifierInformationSalarie() {
+		cout << "Nouveau nom : ";
+		cin >> this->nomDeSalarie;
+		cout << "Nouveau prenom : ";
+		cin >> this->prenomDeSalarie;
+		cout << "Nouveau salaire : ";
+		cin >> this->valSalarie;
 	}
 	//**des fonction
 	void ajouterMoisPayePourSalaries() {
-	
+
 	}
 };
 void rechercheUnSalarie() {
-	
-	}
-	
+
+}
+
 #endif // INCLUDED
